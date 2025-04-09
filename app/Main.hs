3a9e7f1069a5b0ec56a02c1f93ad449ac8a1dba7
AudunKristian <123725511+AudunKristian@users.noreply.github.com>
2025-04-09 12:09:02 +0200

    Image diplays

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,29 @@
-module Main where
+module Main (main) where
+
+import Graphics.Gloss
+
+window :: Display
+window = InWindow "Jump Dash" (800, 600) (10, 10)
+background :: Color
+background = white
+
+player :: Picture
+player = translate (-300) (-40) $
+         Pictures
+           [ color blue $ polygon playerShape
+           , color black $ lineLoop playerShape
+           ]
+  where
+    playerShape = [(0,0), (0,70), (70,70), (70,0)]
+
+ground :: Picture
+ground = translate (-400) (-300) $
+         Pictures
+           [ color (dark (dark blue)) $ polygon groundShape
+           , color black $ lineLoop groundShape
+           ]
+  where
+    groundShape = [(0,0), (0,250), (800,250), (800,0)]
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = display window background (Pictures [ground, player])
