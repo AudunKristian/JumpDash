9493472916f288e47f184541c4311a5e2a539c04
AudunKristian <123725511+AudunKristian@users.noreply.github.com>
2025-04-09 13:16:33 +0200

    Player now correctly jumping

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,63 @@
 module Main (main) where
 
 import Graphics.Gloss
+import Graphics.Gloss.Interface.Pure.Game 
 
 window :: Display
 window = InWindow "Jump Dash" (800, 600) (10, 10)
+
 background :: Color
 background = white
 
-player :: Picture
-player = translate (-300) (-40) $
-         Pictures
-           [ color blue $ polygon playerShape
-           , color black $ lineLoop playerShape
-           ]
-  where
-    playerShape = [(0,0), (0,70), (70,70), (70,0)]
+playerShape :: [(Float, Float)]
+playerShape = [(0, 0), (0, 70), (70, 70), (70, 0)]
 
-ground :: Picture
-ground = translate (-400) (-300) $
-         Pictures
-           [ color (dark (dark blue)) $ polygon groundShape
-           , color black $ lineLoop groundShape
-           ]
-  where
-    groundShape = [(0,0), (0,260), (800,260), (800,0)]
+data GameState = GameState
+  { playerPos :: (Float, Float),  -- (x, y)
+    isJumping :: Bool,            -- Whether the player is currently jumping
+    velocityY :: Float,           -- Vertical speed (used for gravity effect)
+    jumpHeight :: Float           -- Maximum jump height
+  }
+
+initialState :: GameState
+initialState = GameState { playerPos = (-300, -40), isJumping = False, velocityY = 0, jumpHeight = 200 }
+
+groundShape :: [(Float, Float)]
+groundShape = [(0, 0), (0, 260), (800, 260), (800, 0)]
+
+draw :: GameState -> Picture
+draw state = Pictures
+  [ translate (-400) (-300) $ 
+    Pictures
+      [ color (dark (dark blue)) $ polygon groundShape
+      , color black $ lineLoop groundShape
+      ]
+  , let (px, py) = playerPos state
+    in translate px py $ 
+       Pictures
+         [ color blue $ polygon playerShape
+         , color black $ lineLoop playerShape
+         ]
+  ]
+
+handleEvent :: Event -> GameState -> GameState
+handleEvent (EventKey (SpecialKey KeySpace) Down _ _) state
+  | not (isJumping state) = state { isJumping = True, velocityY = 20 } 
+  | otherwise = state  
+handleEvent _ state = state
+
+-- Update game state (gravity, jump mechanics, etc.)
+update :: Float -> GameState -> GameState
+update _ state
+  | isJumping state =  
+      let (px, py) = playerPos state
+          newY = py + velocityY state  
+          newVelocityY = velocityY state - 1  
+      in if newY <= -40 then
+           state { isJumping = False, velocityY = 0, playerPos = (px, -40) }
+         else
+           state { velocityY = newVelocityY, playerPos = (px, newY) }
+  | otherwise = state
 
 main :: IO ()
-main = display window background (Pictures [ground, player])
+main = play window background 60 initialState draw handleEvent update
