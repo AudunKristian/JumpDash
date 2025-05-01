b66f719973507965c01d1b309074f92ad797adb7
AudunKristian <123725511+AudunKristian@users.noreply.github.com>
2025-05-01 10:23:03 +0200

    Added progress

--- app/Main.hs
+++ app/Main.hs
@@ -6,3 +6,18 @@ import Graphics.Gloss.Interface.Pure.Game
 window :: Display
 window = InWindow "Jump Dash" (800, 600) (10, 10)
 
+background :: Color
+background = white
+
+player ::  Picture
+player = color blue $ rectangleSolid 70 70
+
+ground :: Picture
+ground = translate 0 (-230) $ color green $ rectangleSolid 300 400
+
+objectsDrawing :: Picture
+objectsDrawing = pictures [player, ground]
+
+main :: IO ()
+main = display window background objectsDrawing
+
