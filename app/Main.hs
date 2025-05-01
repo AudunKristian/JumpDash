4b9cbbfea3853aaef9dcb7853d3db9529bc1ef82
AudunKristian <123725511+AudunKristian@users.noreply.github.com>
2025-05-01 11:18:41 +0200

    Progress

--- app/Main.hs
+++ app/Main.hs
@@ -4,20 +4,43 @@ import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 
 window :: Display
-window = InWindow "Jump Dash" (800, 600) (10, 10)
+window = InWindow "Jump Dash" (1000, 600) (10, 10)
 
 background :: Color
 background = white
 
-player ::  Picture
-player = color blue $ rectangleSolid 70 70
+type Position = (Float, Float)
 
-ground :: Picture
-ground = translate 0 (-230) $ color green $ rectangleSolid 300 400
+data GameState = GameState
+  { playerPos :: Position
+  , playerVel :: Float
+  }
 
-objectsDrawing :: Picture
-objectsDrawing = pictures [player, ground]
+initialState :: GameState
+initialState = GameState (-350, -15) 40
 
+drawGame :: GameState -> Picture
+drawGame gs = pictures
+  [ translate x y $ color blue $ rectangleSolid 70 70
+  , translate 0 (-200) $ color (dark (dark blue)) $ rectangleSolid 1000 300
+  ]
+  where (x, y) = playerPos gs
+
+handleInput :: Event -> GameState -> GameState
+handleInput _ gs = gs
+
+
+main :: IO ()
+main = play window background 60 initialState drawGame handleInput updateGame
+
+{-
 main :: IO ()
 main = display window background objectsDrawing
 
+player ::  Picture
+player = translate (-350) (-15) $ color blue $ rectangleSolid 70 70
+
+ground :: Picture
+ground = translate 0 (-200) $ color (dark (dark blue)) $ rectangleSolid 1000 300
+
+-}
